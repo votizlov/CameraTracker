bc19854b634ebfea1e2c9038035661a3e919a7e3
Maxim Sviridov <m.sviridov@devar.org>
2020-04-16 03:05:42 +0300

    Started writing animation curve controller

---
+++ Assets/Scripts/AnimationStuff/AnimationConstructor.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace AnimationStuff
+{
+    public class AnimationConstructor : MonoBehaviour
+    {
+        public AnimationClip GetAnimationClipFromKeyFrames(List<DataStructs.Keyframe> keyframes)
+        {
+            var animClip = new AnimationClip();
+
+            var previousCurve = AnimationCurve.Constant(0, 0, 0);
+            
+            animClip.SetCurve("", typeof(Transform), "localPosition.x", 
+                previousCurve);
+            
+            
+            
+            
+            //todo : CalculateFrameTime, SetAll3Curves.
+            
+            for (int i = 1; i < keyframes.Count; i++)
+            {
+                var curvX = AnimationCurve.Linear(previousCurve.keys[0].time, keyframes[i - 1].Position.x, i,
+                    keyframes[i].Position.x);
+                
+                animClip.SetCurve("", typeof(Transform), "localPosition.x", curvX);
+
+                previousCurve = curvX;
+            }
+            
+            
+            
+
+            return animClip;
+        }
+    }
+}
+
+
---
+++ Assets/Scripts/DataStructs/Keyframe.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace DataStructs
+{
+    public struct Keyframe
+    {
+
+        public Vector3 Position;
+        public Quaternion Rotation;
+        public int FrameIndex;
+        
+        
+        
+        
+        internal Keyframe(Vector3 position, Quaternion rotation, int frameIndex)
+        {
+            Position = position;
+            Rotation = rotation;
+            FrameIndex = frameIndex;
+        }
+
+        internal void SetPosition(Vector3 position) => Position = position;
+        
+        internal void SetRotation(Quaternion rotation) => Rotation = rotation;
+
+        internal void SetFrameIndex(int index) => FrameIndex = index;
+
+    }
+}
+
+
