ca99a6e73bdcbc1256d875eb08f20b46d02f6721
votizlov <35692043+votizlov@users.noreply.github.com>
2020-04-19 14:33:08 +0300

    ADDED record ui, anim exporter & recorder

---
+++ Assets/Scripts/AnimationStuff/AnimationExporter.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using UnityEditor.Formats.Fbx.Exporter;
+using UnityEngine;
+
+namespace AnimationStuff
+{
+    public class AnimationExporter : MonoBehaviour
+    {
+        public AnimationRecorder recorder;
+
+        private void Awake()
+        {
+            recorder.onRecordEnd += ExportGameObjects;
+        }
+
+        private void OnDisable()
+        {
+            recorder.onRecordEnd -= ExportGameObjects;
+        }
+
+        public void ExportGameObjects()
+        {
+            Debug.Log("Exporting anim to fbx");
+            string filePath = Path.Combine(Application.dataPath, "Camera.fbx");
+            ModelExporter.ExportObject(filePath, gameObject);
+
+            // ModelExporter.ExportObject can be used instead of 
+            // ModelExporter.ExportObjects to export a single game object
+        }
+
+    }
+}
---
+++ Assets/Scripts/AnimationStuff/AnimationRecorder.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using UnityEditor.Animations;
+using UnityEngine;
+
+namespace AnimationStuff
+{
+    public class AnimationRecorder : MonoBehaviour
+    {
+        private bool isRecording;
+        public float recordInterval;
+        public event Action onRecordEnd;
+        public Animation animation;
+        public Animator animator;
+
+        private GameObjectRecorder recorder;
+
+        private AnimationClip clip;
+    
+        void Start()
+        {
+            clip = new AnimationClip();
+            recorder = new GameObjectRecorder(gameObject);
+            recorder.BindComponentsOfType<Transform>(gameObject,false);
+        }
+
+        public void toggleRecording()
+        {
+            if (isRecording){ isRecording = false; return;}
+
+            isRecording = true;
+            Debug.Log("Recording start");
+            StartCoroutine(Record());
+        }
+
+        private IEnumerator Record()
+        {
+            while (isRecording)
+            {
+                recorder.TakeSnapshot(Time.deltaTime);
+                yield return new WaitForSeconds(recordInterval);
+            }
+            Debug.Log("RecordingEnd");
+            recorder.SaveToClip(clip);
+            //animation.clip = clip;
+            //onRecordEnd.Invoke();
+            //animation.Play(PlayMode.StopSameLayer);
+            recorder.ResetRecording();
+        }
+    }
+}
