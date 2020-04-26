7bda185ce9c23c18f677a130e5942524cf1248cb
votizlov <35692043+votizlov@users.noreply.github.com>
2020-04-27 01:33:35 +0300

    Cleared shit, added readme

--- Assets/Scripts/AnimationStuff/EditorAnimRecorder.cs
+++ Assets/Scripts/AnimationStuff/EditorAnimRecorder.cs
@@ -1,50 +1,44 @@
 ﻿#if UNITY_EDITOR
-using System.Collections;
-using System.Collections.Generic;
-using AnimationStuff;
-using UnityEngine;
-using UnityEditor;
 using UnityEditor.Animations;
+using UnityEngine;
 
 //Я ЗА ЭТОТ КОД НЕ ОТВЕЧАЮ
-public class EditorAnimRecorder : MonoBehaviour
+namespace AnimationStuff
 {
-    public AnimationClip clip;
-    public CameraAnimationExporter exporter;
-    public bool toExportFbx;
-    public GameObject objToExport;
-
-    private GameObjectRecorder m_Recorder;
-
-    void Start()
+    public class EditorAnimRecorder : MonoBehaviour
     {
-        // Create recorder and record the script GameObject.
-        m_Recorder = new GameObjectRecorder(gameObject);
+        public AnimationClip clip;
 
-        // Bind all the Transforms on the GameObject and all its children.
-        m_Recorder.BindComponentsOfType<Transform>(gameObject, true);
-        if(toExportFbx)
-            exporter.exportObject(objToExport);
-    }
+        private GameObjectRecorder m_Recorder;
 
-    void LateUpdate()
-    {
-        if (clip == null)
-            return;
-
-        // Take a snapshot and record all the bindings values for this frame.
-        m_Recorder.TakeSnapshot(Time.deltaTime);
-    }
-
-    void OnDisable()
-    {
-        if (clip == null)
-            return;
-
-        if (m_Recorder.isRecording)
+        void Start()
         {
-            // Save the recorded session to the clip.
-            //m_Recorder.SaveToClip(clip);
+            // Create recorder and record the script GameObject.
+            m_Recorder = new GameObjectRecorder(gameObject);
+
+            // Bind all the Transforms on the GameObject and all its children.
+            m_Recorder.BindComponentsOfType<Transform>(gameObject, true);
+        }
+
+        void LateUpdate()
+        {
+            if (clip == null)
+                return;
+
+            // Take a snapshot and record all the bindings values for this frame.
+            m_Recorder.TakeSnapshot(Time.deltaTime);
+        }
+
+        void OnDisable()
+        {
+            if (clip == null)
+                return;
+
+            if (m_Recorder.isRecording)
+            {
+                // Save the recorded session to the clip.
+                m_Recorder.SaveToClip(clip);
+            }
         }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # CameraTracker
+1. Create new animation with RMB(important!) and link it to root of dummy camera(EditorAnimRecorder component) at AnimationDemo scene.
+2. Record camera animation
+3. After stop recording play animation whole, then stop app.
+4. Put created animation in the animator of dummy camera(important!), then RMB->exportToFBX
+5. ???
+6. PROFIT
  
